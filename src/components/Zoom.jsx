96c2c8275bd7cbeccec9588b0597d6fcee107326
Robert Morrissey <rbrtmorrissey86@gmail.com>
2019-09-08 11:57:17 -0400

    spring animation on pan and zoom

--- src/components/Zoom.jsx
+++ src/components/Zoom.jsx
@@ -1,33 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useSpring, animated} from 'react-spring';
 
 const src = 'https://images.unsplash.com/photo-1444065381814-865dc9da92c0'
 
 const style = {
   backgroundImage: `url(${src})`,
-  transform: 'scale(1)',
+
   overflow: "hidden"
 }
 
 export default function Zoom() {
-  const [backgroundPosition, setBackgroundPosition] = useState('0% 0%')
   const [scale, setScale] = useState(1)
+
+  const [animatedStyle, setAnimatedStyle] = useSpring(() => (
+    { 
+      transform: `scale(${1})`,
+      backgroundPosition: '0% 0%'
+    }
+  ))
 
   const handleClick = e => {
     e.preventDefault()
     const { left, top, width, height } = e.target.getBoundingClientRect()
     const x = (e.pageX - left) / width * 100
     const y = (e.pageY - top) / height * 100
+    setAnimatedStyle({
+      transform: `scale(${scale + .1})`,
+      backgroundPosition: `${x}% ${y}%`
+    })
     setScale(scale + .1)
-    setBackgroundPosition(`${x}% ${y}%`)
   }
 
-  const calculatedStyles = { ...style, backgroundPosition, transform: `scale(${scale})`}
+  const calculatedStyles = { ...style, ...animatedStyle}
   console.log(calculatedStyles);
   
   return (
     <div onClick={handleClick} style={{overflow: "hidden", height: "600px", width: "800px"}}>
-      <div className="image" style={calculatedStyles} >
-      </div>
+      <animated.div className="image" style={calculatedStyles} >
+      </animated.div>
     </div>
   )
 }
